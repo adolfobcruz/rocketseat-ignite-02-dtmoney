d18abdb23b2df8645d95aafeee124ef2448d99e5
Adolfo Cruz <adolfo@rewrite.com.br>
2021-03-18 10:54:06 -0400

    :sparkles: feat: NewTransactionModal + form

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -53,4 +53,27 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  .react-modal-content {
+    position: relative;
+    width: 100%;
+    max-width: 576px;
+    padding: 3rem;
+    border-radius: 0.25rem;
+    background: var(--background);
+  }
 `
