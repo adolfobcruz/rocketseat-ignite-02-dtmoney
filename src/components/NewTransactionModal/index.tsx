1bd836c98099defa42b8108ea91c57ad35fdf656
Adolfo Cruz <adolfo@rewrite.com.br>
2021-03-19 10:49:59 -0400

    :sparkles: feat: NewTransactionModal (form + API)

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
+import { api } from '../../services/api';
 
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -22,12 +23,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type,
-    })
+    };
+
+    api.post('/transactions', data)
   }
 
   return (
