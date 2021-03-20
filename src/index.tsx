35f45e9441a965050786acf719ececedb5363372
Adolfo Cruz <adolfo@rewrite.com.br>
2021-03-20 16:38:34 -0400

    :bug: fix: changed Mirage JS model name + readme

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,30 @@ import { App } from './App';
 
 createServer({
   models: {
-    transition: Model,
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-03-19 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 650,
+          createdAt: new Date('2021-03-21 11:00:00'),
+        },
+      ],
+    })
   },
 
   routes() {
