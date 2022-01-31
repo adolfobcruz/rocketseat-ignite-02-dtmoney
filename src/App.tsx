997655e81491288e8ad1190ea6c0ecad41441a8a
Adolfo Cruz <adolfo@rewrite.com.br>
2022-01-31 17:43:10 -0400

    :sparkles: feat: TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState }             from "react";
-import { Dashboard }            from "./components/Dashboard";
-import { Header }               from "./components/Header";
-import { NewTransactionModal }  from "./components/NewTransactionModal";
-import { TransactionsProvider } from "./TransactionsContext";
+import { useState }             from 'react';
+import { Dashboard }            from './components/Dashboard';
+import { Header }               from './components/Header';
+import { NewTransactionModal }  from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 
 import Modal                    from 'react-modal';
 
-import { GlobalStyle }          from "./styles/global";
+import { GlobalStyle }          from './styles/global';
 
 Modal.setAppElement('#root');
 
